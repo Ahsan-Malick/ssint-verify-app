ef49e0ca12a4b50d302ee15ffce15b1bf32bac09
Ahsan Malik <ahsanjaved74@yahoo.com>
2025-01-06 19:42:16 +0000

    meta data title mod.

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "ssint-registry-app",
+  title: "SSINT. Registry",
   description: "Verified, Secured, Trusted",
   icons: {
     icon: [
